2cad8edc7876f1d99faf46aa078ca047e928a0f1
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-23 06:57:12 +0545

    kyai changes garing

--- src/tests/MyUnitTest.js
+++ src/tests/MyUnitTest.js
@@ -12,7 +12,7 @@ testCases.forEach((testCase) => {
     const result = CustomAdd(testCase.input[0], testCase.input[1]);
 
     if(result === testCase.expectedOutput) {
-        console.log(`Test passed: ${testCase.input} added correctly`);
+        console.log(`Test passed: ${testCase.input} added correctly jhakas`);
     }else{
         console.error(
             `Test failed: Expected ${testCase.expectedOutput}, got ${result}`
