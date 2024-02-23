b189db6c7ba657006c76311c4fcefc833d58eb7a
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-23 08:00:53 +0545

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
